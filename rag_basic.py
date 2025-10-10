dbc1bd180c256afca0caaddc1b7a91e6a9dc821a
Saksham932007 <sakshamkapoor810@gmail.com>
2025-10-10 21:12:08 +0530

    Add basic RAG script structure with imports and TODOs

---
+++ rag_basic.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+RAG Pipeline with Ollama and LangChain
+Basic structure and imports
+"""
+
+import os
+import sys
+
+# Import LangChain components
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+
+print("--- RAG Pipeline with Ollama and LangChain ---")
+print("Setting up basic structure...")
+
+# TODO: Add document loading
+# TODO: Add text splitting  
+# TODO: Add vector store
+# TODO: Add RAG chain
+# TODO: Add chat interface
+
+if __name__ == "__main__":
+    print("RAG Pipeline - Under Development")
